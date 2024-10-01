6b93527673544ac9393fb3b4850df506a8d6b625
Atharva Wagh <gameboy369065@gmail.com>
2024-10-01 17:28:19 +0530

    Update index.py

--- backend/index.py
+++ backend/index.py
@@ -13,7 +13,7 @@ import spacy
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://airesumeanalyser.onrender.com"}})
+CORS(app)
 
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
